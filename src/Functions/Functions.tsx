52f141ca98c9a973fbf253f4846842afe0a9fedb
Hossam Ahmed <hoss.ahmed.dev@gmail.com>
2025-07-07 06:55:34 +0300

    Updated Functions.tsx

--- src/Functions/Functions.tsx
+++ src/Functions/Functions.tsx
@@ -110,7 +110,7 @@ export const addChatUser = async ({ HomeMessages, HomeInputs, router, setAddLoad
   setAddLoadingState(true);
   
   try {
-    const response = await fetch('/api/user', {
+    const response = await fetch('http://127.0.0.1:8000//api/user', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
